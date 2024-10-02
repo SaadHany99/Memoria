e7ec9320bc48931b6f047786838d9910cf62f6a2
SaadHany99 <159723036+SaadHany99@users.noreply.github.com>
2024-10-02 13:43:28 +0300

    Add project files.

---
+++ Medical History/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medical_History.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
